c84baf6edcd08e0b1ef23df057f24b1ce87a9869
Senko Rasic <senko.rasic@dobarkod.hr>
2013-07-26 11:43:18 +0200

    tweaks, docstring and tests for stormpath.Error

--- stormpath/error.py
+++ stormpath/error.py
@@ -1,24 +1,35 @@
 class Error(RuntimeError):
+    """Error returned from the StormPath API service.
+
+    The string content of the error is the low-level message, intended for
+    the developer. The error also contains the following attributes:
+
+    :py:attr:`status` - The corresponding HTTP status code.
+    :py:attr:`code` - A Stormpath-specific error code that can be used to
+        obtain more information.
+    :py:attr:`developer_message` - A clear, plain text explanation with
+        technical details that might assist a developer calling the
+        Stormpath API.
+    :py:attr:`message` - A simple, easy to understand message that you can
+        show directly to your application end-user.
+    :py:attr:`more_info` - A fully qualified URL that may be accessed to
+        obtain more information about the error.
+
+    """
     def __init__(self, error):
-        super(Error, self).__init__(error.get('developerMessage'))
-        self.error = error
+        if error is None:
+            error = {}
 
-    @property
-    def status(self):
-        return self.error.get('status', -1)
+        def try_int(val):
+            try:
+                return int(val)
+            except:
+                return -1
 
-    @property
-    def code(self):
-        return self.error.get('code', -1)
-
-    @property
-    def developer_message(self):
-        return self.error.get('developer_message')
-
-    @property
-    def more_info(self):
-        return self.error.get('more_info')
-
-    @property
-    def message(self):
-        return self.error.get('message')
+        msg = error.get('developerMessage')
+        super(Error, self).__init__(msg)
+        self.status = try_int(error.get('status', -1))
+        self.code = try_int(error.get('code', -1))
+        self.developer_message = msg
+        self.more_info = error.get('moreInfo')
+        self.message = error.get('message')
