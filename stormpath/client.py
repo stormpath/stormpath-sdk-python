329e0482e7551fb0b738d80ede5ebeb7de6919cb
Senko Rasic <senko.rasic@dobarkod.hr>
2013-07-30 00:16:45 +0200

    refactored client based on the resource refactoring

--- stormpath/client.py
+++ stormpath/client.py
@@ -1,105 +1,37 @@
 from .auth import Auth
-from .resource import ResourceList
-from .resource import Tenant
-from .resource import Application, ApplicationResourceList
-from .resource import Directory
-from .resource import Account
-from .resource import Group, GroupMembership
-from .resource import API_URL
+from .http import HttpExecutor
+from .data_store import DataStore
+from .resource.tenant import Tenant
+from .resource.account import AccountList
+from .resource.group import GroupList
+from .resource.group_membership import GroupMembershipList
 
 
 class Client(object):
-    """
-    Client is the main entry point for consuming Stormpath REST API.
-    It takes care of setting up authentication/session and provides
-    access to Stormpath resources.
+    BASE_URL = 'https://api.stormpath.com/v1'
 
-    Resources:
-    https://www.stormpath.com/docs/rest/api#Resources
-
-    """
     def __init__(self, **kwargs):
-        self.auth = Auth(**kwargs)
-        self._tenant = None
-
-    def _get_tenant(self):
-        if not self._tenant:
-            self._tenant = Tenant(auth=self.auth, id='current')
-        return self._tenant
-
-    @property
-    def tenant(self):
-        """
-        Returns current tenant used by Client to find related resources.
-
-        https://www.stormpath.com/docs/rest/api#Tenants
-
-        """
-        return self._get_tenant()
+        # FIXME - can't use digest yet
+        executor = HttpExecutor(self.BASE_URL, Auth(**kwargs).basic)
+        self.data_store = DataStore(executor)
+        self.tenant = Tenant(client=self, href='/tenants/current')
 
     @property
     def applications(self):
-        """
-        Returns applications related to current tenant used by Client.
-
-        https://www.stormpath.com/docs/rest/api#Applications
-
-        """
-        url = self._get_tenant().applications.get('href')
-        return ApplicationResourceList(url=url, auth=self.auth,
-            resource=Application)
+        return self.tenant.applications
 
     @property
     def directories(self):
-        """
-        Returns directories related to current tenant used by Client.
-
-        https://www.stormpath.com/docs/rest/api#Directories
-
-        """
-        url = self._get_tenant().directories.get('href')
-        return ResourceList(url=url, auth=self.auth, resource=Directory)
+        return self.tenant.directories
 
     @property
     def accounts(self):
-        """
-        Returns accounts related to current tenant used by Client.
-
-        https://www.stormpath.com/docs/rest/api#Accounts
-
-        """
-        # The API doesn't return the accounts URL along with the Tenant.
-        # If the SDK user wants to access an account regardless of directory
-        # or application, he/she has to follow a direct url but we still have to
-        # create the Tenant to access applications and directories.
-        self._get_tenant()
-        url = '%s%s' % (API_URL, "accounts")
-        return ResourceList(url=url, auth=self.auth, resource=Account)
+        return AccountList(self, href='/accounts')
 
     @property
     def groups(self):
-        """
-        Returns groups related to current tenant used by Client.
-
-        https://www.stormpath.com/docs/rest/api#Groups
-
-        """
-        # The API doesn't return the groups URL along with the Tenant.
-        # If the SDK user wants to access a group regardless of directory
-        # or application, he/she has to follow a direct url but we still have to
-        # create the Tenant to access applications and directories.
-        self._get_tenant()
-        url = '%s%s' % (API_URL, "groups")
-        return ResourceList(url=url, auth=self.auth, resource=Group)
+        return GroupList(self, href='/groups')
 
     @property
     def group_memberships(self):
-        """
-        Returns group memperships related to current tenant used by Client.
-
-        https://www.stormpath.com/docs/rest/api#GroupMemberships
-
-        """
-        self._get_tenant()
-        url = '%s%s' % (API_URL, "groupMemberships")
-        return ResourceList(url=url, auth=self.auth, resource=GroupMembership)
+        return GroupMembershipList(self, href='/groupMemberships')
