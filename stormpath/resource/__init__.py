16291f8220b0ff530e8bae7ff6e9bde783870a36
Senko Rasic <senko.rasic@dobarkod.hr>
2013-07-30 00:14:37 +0200

    no need to autoload everything into resource
    As the library user will rarely need to use the resource classes
    directly.

--- stormpath/resource/__init__.py
+++
@@ -1,7 +0,0 @@
-from .base import Resource, ResourceList, Expansion, API_URL
-from .tenant import Tenant
-from .account import Account, AccountResourceList
-from .group import Group, GroupResourceList
-from .group_membership import GroupMembership, GroupMembershipResourceList
-from .directory import Directory
-from .application import Application, ApplicationResourceList
