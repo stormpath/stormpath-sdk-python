70ac7ac87756b41266b306e0f8ec3e462f4b059f
gcetusic <goran.cetusic@gmail.com>
2013-07-30 16:53:46 +0200

    easier imports

---
+++ stormpath/resource/__init__.py
@@ -0,0 +1,7 @@
+from .account import Account, AccountList
+from .group import Group, GroupList
+from .tenant import Tenant
+from .directory import Directory
+from .group_membership import GroupMembership, GroupMembershipList
+from .base import Expansion, Resource, ResourceList
+from .password_reset_token import PasswordResetTokenList
