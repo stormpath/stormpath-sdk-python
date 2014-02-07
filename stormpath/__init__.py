fd0122d07739e9ec0a47c1bf5d26554b172e0104
Senko Rasic <senko.rasic@dobarkod.hr>
2014-02-07 22:25:26 +0100

    update copyright notice

--- stormpath/__init__.py
+++ stormpath/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'Senko Rasic'
-__copyright__ = 'Copyright 2012,2013 StormPath, Inc.'
+__copyright__ = 'Copyright 2012-2014 Stormpath, Inc.'
 
 __version_info__ = ('1', '0', '0', 'beta', '1')
 __version__ = '.'.join(__version_info__)
