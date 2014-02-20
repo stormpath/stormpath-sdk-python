094cca4a736a9518d2c1bf3ac6d53b4b97e97038
Goran Cetusic <goran.cetusic@gmail.com>
2014-02-20 14:31:01 +0100

    updated version

--- stormpath/__init__.py
+++ stormpath/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Senko Rasic'
 __copyright__ = 'Copyright 2012,2013 StormPath, Inc.'
 
-__version_info__ = ('1', '0', '0', 'beta', '1')
+__version_info__ = ('1', '0', '1')
 __version__ = '.'.join(__version_info__)
-__short_version__ = '.'.join(__version_info__[:-2])
+__short_version__ = __version__
