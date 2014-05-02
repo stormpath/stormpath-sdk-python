86b35d36cdd6f3dd80b73ed2e9a7836a59a7da41
Randall Degges <r@rdegges.com>
2014-05-02 11:18:53 -0700

    Bumping version number. (1.2.0)

--- stormpath/__init__.py
+++ stormpath/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Stormpath, Inc.'
 __copyright__ = 'Copyright 2012-2014 Stormpath, Inc.'
 
-__version_info__ = ('1', '1', '0')
+__version_info__ = ('1', '2', '0')
 __version__ = '.'.join(__version_info__)
 __short_version__ = '.'.join(__version_info__)
