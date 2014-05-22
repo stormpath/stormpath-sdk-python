6b8ec780272ae4ad611b0dea39033bbc06c790ce
Randall Degges <r@rdegges.com>
2014-05-22 12:58:29 -0700

    Fixing spacing.

--- stormpath/client.py
+++ stormpath/client.py
@@ -42,8 +42,7 @@ class Client(object):
     """
     BASE_URL = 'https://api.stormpath.com/v1'
 
-    def __init__(self, cache_options=None, expand=None,
-            proxies=None, **auth_kwargs):
+    def __init__(self, cache_options=None, expand=None, proxies=None, **auth_kwargs):
         executor = HttpExecutor(self.BASE_URL, Auth(**auth_kwargs).scheme,
             proxies)
         self.data_store = DataStore(executor, cache_options)
