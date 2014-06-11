ac4d1dfb272acdcd50e70709c9b66ecaab4c7470
Deni Bertovic <deni@kset.org>
2014-06-11 11:10:43 +0200

    fix api keys creation

--- stormpath/resources/api_key.py
+++ stormpath/resources/api_key.py
@@ -28,7 +28,6 @@ class ApiKey(Resource, DeleteMixin, SaveMixin, StatusMixin):
 
 class ApiKeyList(CollectionResource):
     """Application resource list."""
-    create_path = '/apiKeys'
     resource_class = ApiKey
 
     def get_key(self, client_id):
@@ -37,3 +36,18 @@ class ApiKeyList(CollectionResource):
         except IndexError:
             return False
 
+    def create(self, expand=None):
+        resource_attrs = self.resource_class.get_resource_attributes()
+        data = {}
+        params = {}
+        if expand:
+            params.update({'expand': expand.get_params()})
+
+        return self.resource_class(
+            self._client,
+            properties=self._store.create_resource(
+                self._get_create_path(),
+                data,
+                params=params
+            )
+        )
