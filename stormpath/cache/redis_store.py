9df9c8fe441f54874f3e6a7e36b2279e0751a273
Deni Bertovic <deni@kset.org>
2014-12-01 16:28:05 +0100

    add doc strings for RedisStore

--- stormpath/cache/redis_store.py
+++ stormpath/cache/redis_store.py
@@ -10,7 +10,32 @@ from .entry import CacheEntry
 
 
 class RedisStore(object):
-    """Caching implementation that uses Redis as data storage."""
+    """Caching implementation that uses Redis as data storage.
+
+    :param host: String representing the hostname or IP of the Redis server
+
+    :param port: Port number (int) on which the Redis server is listening
+
+    :param db: DB querystring option (Default: 0, e.g. redis://localhost?db=0)
+
+    :param password: Redis server password (Default: No Password)
+
+    :param socket_timeout: Connection timeout to Redis server
+
+    :param connection_pool: ConnectionPool for Redis instance (See redis-py docs)
+
+    :param charset: Default character set
+
+    :param errors: Default error settings
+
+    :param decode_responses: Default: False (values in message dictionaries will be
+        byte strings (str on Python 2, bytes on Python 3)
+
+    :param unix_socket_path: Socker path. For using UnixDomainSocketConnection
+        (see redis-py docs for more details)
+
+    :param ttl: Default TTL
+    """
 
     DEFAULT_TTL = 5 * 60  # seconds
 
