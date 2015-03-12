e8711fc8a2c329a9e79fbb4da0370ceb39c97913
Randall Degges <r@rdegges.com>
2015-03-12 10:06:07 -0700

    Version bump.

--- stormpath/__init__.py
+++ stormpath/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Stormpath, Inc.'
 __copyright__ = 'Copyright 2012-2014 Stormpath, Inc.'
 
-__version_info__ = ('2', '0', '1')
+__version_info__ = ('2', '0', '2')
 __version__ = '.'.join(__version_info__)
 __short_version__ = '.'.join(__version_info__)
