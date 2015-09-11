62956b8eff1c5948469d4a22fa12bd1cd1cceeab
Ana Vojnovic <ana.vojnovic@dobarkod.hr>
2015-09-11 19:45:02 +0200

    Added some live tests to improve coverage.

--- tests/live/test_client_auth.py
+++ tests/live/test_client_auth.py
@@ -18,6 +18,25 @@ class TestAuth(LiveBase):
         # force the SDK to make a call to the server
         list(client.applications)
 
+    def test_basic_authentication_with_api_key_succeeds(self):
+        api_key = {
+            'api_key_id': self.api_key_id,
+            'api_key_secret': self.api_key_secret
+        }
+        client = Client(
+            api_key=api_key,
+            scheme='basic')
+        # force the SDK to make a call to the server
+        list(client.applications)
+
+    def test_basic_authentication_w_api_key_id_n_api_key_secret_succeeds(self):
+        client = Client(
+            api_key_id=self.api_key_id,
+            api_key_secret=self.api_key_secret,
+            scheme='basic')
+        # force the SDK to make a call to the server
+        list(client.applications)
+
     def test_basic_authentication_fails(self):
         client = Client(
             id=self.api_key_id + 'x',
