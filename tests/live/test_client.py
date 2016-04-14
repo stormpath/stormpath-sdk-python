bc087c4791bc3cb39c8fc0f994cd181e2769140f
Randall Degges <r@rdegges.com>
2016-04-14 01:35:48 -0400

    Adding more robust tests for `Client.api_keys` property.

--- tests/live/test_client.py
+++ tests/live/test_client.py
@@ -1,29 +1,38 @@
 """Live tests of Client functionality."""
 
 
-import datetime
-
-from stormpath.error import Error
-
-from .base import AuthenticatedLiveBase, SingleApplicationBase, AccountBase
-from stormpath.resources import (
-    AccountCreationPolicy, Provider, SamlPolicy, SamlServiceProvider,
-    SsoInitiationEndpoint
-)
-from stormpath.resources.application import Application
-from stormpath.resources.default_relay_state import (
-    DefaultRelayState, DefaultRelayStateList
-)
-from stormpath.resources.tenant import Tenant
-from stormpath.resources.agent import (
-    Agent, AgentConfig, AgentAccountConfig, AgentGroupConfig
-)
-from stormpath.resources.email_template import EmailTemplate
-from stormpath.resources.password_policy import PasswordPolicy
+from .base import AuthenticatedLiveBase
 
 
 class TestClientProperties(AuthenticatedLiveBase):
     """Assert all Client properties work as expected."""
 
     def test_client_api_keys(self):
-        self.assertEqual(len(self.client.api_keys), 1)
+        current_api_keys = len(self.client.api_keys)
+
+        app = self.client.applications.create({
+            'name': self.get_random_name()
+        }, create_directory=True)
+        account = app.accounts.create({
+            'given_name': 'Randall',
+            'surname': 'Degges',
+            'email': self.get_random_name() + '@example.com',
+            'password': 'wootILOVEc00kies!'
+        })
+        key = account.api_keys.create()
+
+        self.assertEqual(len(self.client.api_keys), current_api_keys + 1)
+
+        key.delete()
+
+        self.assertEqual(len(self.client.api_keys), current_api_keys)
+
+        total_keys_to_create = 150
+        for i in range(total_keys_to_create):
+            key = account.api_keys.create()
+
+        self.assertEqual(len(self.client.api_keys), current_api_keys + total_keys_to_create)
+
+        account.delete()
+
+        self.assertEqual(len(self.client.api_keys), current_api_keys)
