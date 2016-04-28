56f88c579deb1d98095486ecc6ef05f38f03a2c2
Randall Degges <r@rdegges.com>
2016-04-28 15:29:58 -0700

    Rewriting tests slightly for clarity.

--- tests/live/test_client.py
+++ tests/live/test_client.py
@@ -3,8 +3,10 @@
 
 from .base import AuthenticatedLiveBase, LiveBase
 
+from stormpath.cache.null_cache_store import NullCacheStore
 from stormpath.client import Client
 from stormpath.error import Error
+from stormpath.resources import Expansion
 
 
 class TestClientInitialization(LiveBase):
@@ -17,65 +19,50 @@ class TestClientInitialization(LiveBase):
         client = Client(id=self.api_key_id, secret=self.api_key_secret, base_url='https://example.com')
         self.assertEqual(client.BASE_URL, 'https://example.com')
 
-    def test_basic_authentication_succeeds(self):
+    def test_cache_options(self):
         client = Client(
-            id=self.api_key_id,
-            secret=self.api_key_secret,
-            scheme='basic')
-        # force the SDK to make a call to the server
+            id = self.api_key_id,
+            secret = self.api_key_secret,
+            cache_options = {
+                'regions': {
+                    'customData': {
+                        'store': NullCacheStore
+                    }
+                }
+            }
+        )
+
+        self.assertIsInstance(client.data_store.cache_manager.caches['customData'].store, NullCacheStore)
+
+    def test_basic_authentication(self):
+        client = Client(id=self.api_key_id, secret=self.api_key_secret, scheme='basic')
         list(client.applications)
 
-    def test_basic_authentication_with_api_key_succeeds(self):
         api_key = {
             'api_key_id': self.api_key_id,
             'api_key_secret': self.api_key_secret
         }
-        client = Client(
-            api_key=api_key,
-            scheme='basic')
-        # force the SDK to make a call to the server
+        client = Client(api_key=api_key, scheme='basic')
         list(client.applications)
 
-    def test_basic_authentication_w_api_key_id_n_api_key_secret_succeeds(self):
-        client = Client(
-            api_key_id=self.api_key_id,
-            api_key_secret=self.api_key_secret,
-            scheme='basic')
-        # force the SDK to make a call to the server
+        client = Client(api_key_id=self.api_key_id, api_key_secret=self.api_key_secret, scheme='basic')
         list(client.applications)
 
-    def test_basic_authentication_fails(self):
-        client = Client(
-            id=self.api_key_id + 'x',
-            secret=self.api_key_secret + 'x',
-            scheme='basic')
-
-        # force the SDK to make a call to the server
+        client = Client(id=self.api_key_id + 'x', secret=self.api_key_secret + 'x', scheme='basic')
         with self.assertRaises(Error):
             list(client.applications)
 
-    def test_digest_authentication_succeeds(self):
-        client = Client(
-            id=self.api_key_id,
-            secret=self.api_key_secret,
-            scheme='SAuthc1')
-        # force the SDK to make a call to the server
-        client.applications
+    def test_digest_authentication(self):
+        client = Client(id=self.api_key_id, secret=self.api_key_secret, scheme='SAuthc1')
+        list(client.applications)
 
-    def test_digest_authentication_fails(self):
-        client = Client(
-            id=self.api_key_id + 'x',
-            secret=self.api_key_secret + 'x',
-            scheme='SAuthc1')
-
-        # force the SDK to make a call to the server
+        client = Client(id=self.api_key_id + 'x', secret=self.api_key_secret + 'x', scheme='SAuthc1')
         with self.assertRaises(Error):
             list(client.applications)
 
-    def test_load_from_environment_variables(self):
+    def test_environment_variable_authentication(self):
         client = Client()
-        for app in client.applications:
-            self.assertTrue(app.href)
+        list(client.applications)
 
 
 class TestClientProperties(AuthenticatedLiveBase):
