ab035bbfdefa568e0426352048d4a2ca815b2eb3
Randall Degges <r@rdegges.com>
2016-06-13 11:26:17 -0700

    Cleaning up formatting of provider tests.

--- tests/live/test_provider.py
+++ tests/live/test_provider.py
@@ -2,10 +2,11 @@
 Integration tests for various pieces involved in external provider support.
 """
 
-import json
-from unittest import main
+from json import loads
 from os import getenv
+
 from requests import Session
+
 from tests.live.base import AuthenticatedLiveBase
 from stormpath.error import Error as StormpathError
 from stormpath.resources import (
@@ -20,41 +21,35 @@ from stormpath.resources import (
 class TestProviderDirectories(AuthenticatedLiveBase):
 
     def test_creating_provider_directory_passes_provider_info(self):
-        directory = self.client.directories.create(
-            {
-                'name': self.get_random_name(),
-                'description': 'Testing Google Auth Provider',
-                'provider':
-                    {
-                        'client_id': 'some-client-id',
-                        'client_secret': 'some-client-secret',
-                        'redirect_uri': 'some-redirect-uri',
-                        'provider_id': Provider.GOOGLE
-                    }
-            })
+        directory = self.client.directories.create({
+            'name': self.get_random_name(),
+            'description': 'Testing Google Auth Provider',
+            'provider': {
+                'client_id': 'some-client-id',
+                'client_secret': 'some-client-secret',
+                'redirect_uri': 'some-redirect-uri',
+                'provider_id': Provider.GOOGLE
+            }
+        })
 
-        app = self.client.applications.create(
-            {
-                'name': self.get_random_name(),
-                'description': 'Testing app for Google Auth',
-                'status': 'enabled'
-            })
+        app = self.client.applications.create({
+            'name': self.get_random_name(),
+            'description': 'Testing app for Google Auth',
+            'status': 'enabled'
+        })
 
-        self.client.account_store_mappings.create(
-            {
-                'application': app,
-                'account_store': directory,
-                'list_index': 0,
-                'is_default_account_store': False,
-                'is_default_group_store': False
-            })
+        self.client.account_store_mappings.create({
+            'application': app,
+            'account_store': directory,
+            'list_index': 0,
+            'is_default_account_store': False,
+            'is_default_group_store': False
+        })
 
         with self.assertRaises(StormpathError) as se:
-            app.get_provider_account(
-                provider=Provider.GOOGLE, code='some-code')
+            app.get_provider_account(provider=Provider.GOOGLE, code='some-code')
 
-        self.assertTrue("Google error message: 400 Bad Request" in str(
-            se.exception.developer_message))
+        self.assertTrue("Google error message: 400 Bad Request" in str(se.exception.developer_message))
 
         directory.delete()
         app.delete()
@@ -64,71 +59,59 @@ class TestProviderDirectories(AuthenticatedLiveBase):
         facebook_api_key_secret = getenv('FACEBOOK_API_KEY_SECRET')
 
         if not facebook_api_key_id:
-            self.fail("Please set FACEBOOK_API_KEY_ID environment variable!")
+            self.fail('Please set FACEBOOK_API_KEY_ID environment variable!')
+
         if not facebook_api_key_secret:
-            self.fail(
-                "Please set FACEBOOK_API_KEY_SECRET environment variable!")
+            self.fail('Please set FACEBOOK_API_KEY_SECRET environment variable!')
 
         s = Session()
-        res = s.request(
-            'GET', 'https://graph.facebook.com/oauth/access_token',
-            params={
-                'client_id': facebook_api_key_id,
-                'client_secret': facebook_api_key_secret,
-                'grant_type': 'client_credentials'
-            })
+        res = s.request('GET', 'https://graph.facebook.com/oauth/access_token', params={
+            'client_id': facebook_api_key_id,
+            'client_secret': facebook_api_key_secret,
+            'grant_type': 'client_credentials'
+        })
         access_token = res.text.split('=')[1]
 
-        res = s.request(
-            'GET',
-            'https://graph.facebook.com/%s/accounts/test-users' % (
-                facebook_api_key_id ),
-            params={
-                'installed': 'true',
-                'name': 'Some User',
-                'locale': 'en_US',
-                'permissions': 'read_stream',
-                'method': 'post',
-                'access_token': access_token,
-            })
-        access_token = json.loads(res.text)['access_token']
+        res = s.request('GET', 'https://graph.facebook.com/{}/accounts/test-users'.format(facebook_api_key_id), params={
+            'installed': 'true',
+            'name': 'Some User',
+            'locale': 'en_US',
+            'permissions': 'read_stream',
+            'method': 'post',
+            'access_token': access_token,
+        })
+        access_token = loads(res.text)['access_token']
 
-        directory = self.client.directories.create(
-            {
-                'name': self.get_random_name(),
-                'description': 'Testing Facebook Auth Provider',
-                'provider':
-                    {
-                        'client_id': facebook_api_key_id,
-                        'client_secret': facebook_api_key_secret,
-                        'provider_id': Provider.FACEBOOK
-                    }
-            })
+        directory = self.client.directories.create({
+            'name': self.get_random_name(),
+            'description': 'Testing Facebook Auth Provider',
+            'provider': {
+                'client_id': facebook_api_key_id,
+                'client_secret': facebook_api_key_secret,
+                'provider_id': Provider.FACEBOOK
+            }
+        })
 
-        app = self.client.applications.create(
-            {
-                'name': self.get_random_name(),
-                'description': 'Testing app for Facebook Auth',
-                'status': 'enabled'
-            })
+        app = self.client.applications.create({
+            'name': self.get_random_name(),
+            'description': 'Testing app for Facebook Auth',
+            'status': 'enabled'
+        })
 
-        self.client.account_store_mappings.create(
-            {
-                'application': app,
-                'account_store': directory,
-                'list_index': 0,
-                'is_default_account_store': False,
-                'is_default_group_store': False
-            })
+        self.client.account_store_mappings.create({
+            'application': app,
+            'account_store': directory,
+            'list_index': 0,
+            'is_default_account_store': False,
+            'is_default_group_store': False
+        })
 
         with self.assertRaises(StormpathError) as se:
-            app.get_provider_account(
-                provider=Provider.FACEBOOK, access_token=access_token)
+            app.get_provider_account(provider=Provider.FACEBOOK, access_token=access_token)
 
-        self.assertTrue(
-            "Stormpath is unable to create or update the account because the "
-            "Facebook response did not contain the required 'email' "
-            "property." in str(se.exception.developer_message))
+        self.assertTrue('Stormpath is unable to create or update the account because the '
+            'Facebook response did not contain the required \'email\' '
+            'property.' in str(se.exception.developer_message))
 
         directory.delete()
         app.delete()
@@ -137,40 +120,34 @@ class TestProviderDirectories(AuthenticatedLiveBase):
         github_api_key_id = 'some-api-key-id'
         github_api_key_secret = 'some-api-secret'
 
-        directory = self.client.directories.create(
-            {
-                'name': self.get_random_name(),
-                'description': 'Testing Github Auth Provider',
-                'provider':
-                    {
-                        'client_id': github_api_key_id,
-                        'client_secret': github_api_key_secret,
-                        'provider_id': Provider.GITHUB
-                    }
-            })
+        directory = self.client.directories.create({
+            'name': self.get_random_name(),
+            'description': 'Testing Github Auth Provider',
+            'provider': {
+                'client_id': github_api_key_id,
+                'client_secret': github_api_key_secret,
+                'provider_id': Provider.GITHUB
+            }
+        })
 
-        app = self.client.applications.create(
-            {
-                'name': self.get_random_name(),
-                'description': 'Testing app for Github Auth',
-                'status': 'enabled'
-            })
+        app = self.client.applications.create({
+            'name': self.get_random_name(),
+            'description': 'Testing app for Github Auth',
+            'status': 'enabled'
+        })
 
-        self.client.account_store_mappings.create(
-            {
-                'application': app,
-                'account_store': directory,
-                'list_index': 0,
-                'is_default_account_store': False,
-                'is_default_group_store': False
-            })
+        self.client.account_store_mappings.create({
+            'application': app,
+            'account_store': directory,
+            'list_index': 0,
+            'is_default_account_store': False,
+            'is_default_group_store': False
+        })
 
         with self.assertRaises(StormpathError) as se:
-            app.get_provider_account(
-                provider=Provider.GITHUB, access_token='some-access-token')
+            app.get_provider_account(provider=Provider.GITHUB, access_token='some-access-token')
 
-        self.assertTrue("Github error message: 401 Unauthorized" in str(
-            se.exception.developer_message))
+        self.assertTrue("Github error message: 401 Unauthorized" in str(se.exception.developer_message))
 
         directory.delete()
         app.delete()
@@ -179,40 +156,34 @@ class TestProviderDirectories(AuthenticatedLiveBase):
         linkedin_api_key_id = 'some-api-key-id'
         linkedin_api_key_secret = 'some-api-key-secret'
 
-        directory = self.client.directories.create(
-            {
-                'name': self.get_random_name(),
-                'description': 'Testing LinkedIn Auth Provider',
-                'provider':
-                    {
-                        'client_id': linkedin_api_key_id,
-                        'client_secret': linkedin_api_key_secret,
-                        'provider_id': Provider.LINKEDIN
-                    }
-            })
+        directory = self.client.directories.create({
+            'name': self.get_random_name(),
+            'description': 'Testing LinkedIn Auth Provider',
+            'provider': {
+                'client_id': linkedin_api_key_id,
+                'client_secret': linkedin_api_key_secret,
+                'provider_id': Provider.LINKEDIN
+            }
+        })
 
-        app = self.client.applications.create(
-            {
-                'name': self.get_random_name(),
-                'description': 'Testing app for LinkedIn Auth',
-                'status': 'enabled'
-            })
+        app = self.client.applications.create({
+            'name': self.get_random_name(),
+            'description': 'Testing app for LinkedIn Auth',
+            'status': 'enabled'
+        })
 
-        self.client.account_store_mappings.create(
-            {
-                'application': app,
-                'account_store': directory,
-                'list_index': 0,
-                'is_default_account_store': False,
-                'is_default_group_store': False
-            })
+        self.client.account_store_mappings.create({
+            'application': app,
+            'account_store': directory,
+            'list_index': 0,
+            'is_default_account_store': False,
+            'is_default_group_store': False
+        })
 
         with self.assertRaises(StormpathError) as se:
-            app.get_provider_account(
-                provider=Provider.LINKEDIN, access_token='some-access-token')
+            app.get_provider_account(provider=Provider.LINKEDIN, access_token='some-access-token')
 
-        self.assertTrue("Linkedin error message: Invalid access token." in str(
-            se.exception.developer_message))
+        self.assertTrue('Linkedin error message: Invalid access token.' in str(se.exception.developer_message))
 
         directory.delete()
         app.delete()
@@ -241,20 +212,17 @@ class TestProviderDirectories(AuthenticatedLiveBase):
         -----END CERTIFICATE-----
         """
 
-        directory = self.client.directories.create(
-            {
-                'name': self.get_random_name(),
-                'description': 'Testing SAML Provider',
-                'provider':
-                    {
-                        'sso_login_url': sso_login_url,
-                        'sso_logout_url': sso_logout_url,
-                        'encoded_x509_signing_cert': encoded_x509_signing_cert,
-                        'request_signature_algorithm':
-                            Provider.SIGNING_ALGORITHM_RSA_SHA_256,
-                        'provider_id': Provider.SAML
-                    }
-            })
+        directory = self.client.directories.create({
+            'name': self.get_random_name(),
+            'description': 'Testing SAML Provider',
+            'provider': {
+                'sso_login_url': sso_login_url,
+                'sso_logout_url': sso_logout_url,
+                'encoded_x509_signing_cert': encoded_x509_signing_cert,
+                'request_signature_algorithm': Provider.SIGNING_ALGORITHM_RSA_SHA_256,
+                'provider_id': Provider.SAML
+            }
+        })
 
         self.assertIsNotNone(directory.href)
 
@@ -262,9 +230,7 @@ class TestProviderDirectories(AuthenticatedLiveBase):
         spm = provider.service_provider_metadata
         self.assertIsNotNone(spm.href)
         self.assertIsInstance(spm, SamlServiceProviderMetadata)
-        self.assertIsInstance(
-            spm.assertion_consumer_service_post_endpoint,
-            AssertionConsumerServicePostEndpoint)
+        self.assertIsInstance( spm.assertion_consumer_service_post_endpoint, AssertionConsumerServicePostEndpoint)
         self.assertEqual(provider.provider_id, Provider.SAML)
         self.assertEqual(provider.sso_login_url, sso_login_url)
         self.assertEqual(provider.sso_logout_url, sso_logout_url)
@@ -295,35 +261,25 @@ class TestProviderDirectories(AuthenticatedLiveBase):
         -----END CERTIFICATE-----
         """
 
-        directory = self.client.directories.create(
-            {
-                'name': self.get_random_name(),
-                'description': 'Testing SAML Provider',
-                'provider':
-                    {
-                        'sso_login_url': sso_login_url,
-                        'sso_logout_url': sso_logout_url,
-                        'encoded_x509_signing_cert': encoded_x509_signing_cert,
-                        'request_signature_algorithm':
-                            Provider.SIGNING_ALGORITHM_RSA_SHA_256,
-                        'provider_id': Provider.SAML
-                    },
-            })
+        directory = self.client.directories.create({
+            'name': self.get_random_name(),
+            'description': 'Testing SAML Provider',
+            'provider': {
+                'sso_login_url': sso_login_url,
+                'sso_logout_url': sso_logout_url,
+                'encoded_x509_signing_cert': encoded_x509_signing_cert,
+                'request_signature_algorithm': Provider.SIGNING_ALGORITHM_RSA_SHA_256,
+                'provider_id': Provider.SAML
+            },
+        })
 
         name_format = 'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified'
-        rule1 = AttributeStatementMappingRule(
-            name='name1',
-            name_format=name_format,
-            account_attributes=['customData.name1', 'customData.otherName1'])
-        rule2 = AttributeStatementMappingRule(
-            name='name2',
-            name_format=name_format,
-            account_attributes=['customData.name2'])
+        rule1 = AttributeStatementMappingRule(name='name1', name_format=name_format, account_attributes=['customData.name1', 'customData.otherName1'])
+        rule2 = AttributeStatementMappingRule(name='name2', name_format=name_format, account_attributes=['customData.name2'])
 
         asmr = directory.provider.attribute_statement_mapping_rules
         asmr.items = [rule1]
         asmr.save()
-        asmr.refresh()
 
         self.assertIsInstance(asmr, AttributeStatementMappingRules)
         self.assertEqual(len(asmr.items), 1)
@@ -344,7 +300,3 @@ class TestProviderDirectories(AuthenticatedLiveBase):
         self.assertEqual(items[0].name, 'name2')
 
         directory.delete()
-
-
-if __name__ == '__main__':
-    main()
