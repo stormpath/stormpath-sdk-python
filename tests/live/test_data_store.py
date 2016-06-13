d48e11d90f9a3eaf8edaf9efdae2700fb57e2175
Randall Degges <r@rdegges.com>
2016-06-13 14:50:22 -0700

    Making test python 3 compatible.

--- tests/live/test_data_store.py
+++ tests/live/test_data_store.py
@@ -35,7 +35,7 @@ class TestLiveDataStore(SingleApplicationBase):
             'password': 'wootILOVEc00kies!!<33',
         })
         key = acc.api_keys.create()
-        self.assertIsInstance(key.id, unicode)
+        self.assertIsInstance(key.id, str)
 
         data_store = DataStore(executor=self.executor)
         data = data_store.get_resource(self.app.href + '/apiKeys', {'id': key.id})
