484ba64d486dac98175d920f8a5faa34cce34988
Randall Degges <r@rdegges.com>
2016-07-03 10:37:47 -0700

    Newstyle string formatting.

--- stormpath/nonce.py
+++ stormpath/nonce.py
@@ -1,5 +1,6 @@
 class Nonce(object):
     def __init__(self, value):
         self.value = value
-        # we fake the href to comply with what the data store expects
-        self.href = '//nonces/%s' % value
+
+        # We fake the href to comply with what the data store expects.
+        self.href = '//nonces/{}'.format(value)
