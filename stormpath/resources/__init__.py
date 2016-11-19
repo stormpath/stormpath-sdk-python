5e1a45429583ba8bc4e8c3c26185858b80902534
Randall Degges <r@rdegges.com>
2016-11-18 16:30:50 -0800

    Reordering imports for OCD.

--- stormpath/resources/__init__.py
+++ stormpath/resources/__init__.py
@@ -5,33 +5,25 @@ from .account import Account, AccountList
 from .account_creation_policy import AccountCreationPolicy
 from .account_schema import AccountSchema
 from .account_store import AccountStore
-from .assertion_consumer_service_post_endpoint import (
-    AssertionConsumerServicePostEndpoint
-)
-from .attribute_statement_mapping_rule import (
-    AttributeStatementMappingRule,
-    AttributeStatementMappingRules
-)
+from .assertion_consumer_service_post_endpoint import AssertionConsumerServicePostEndpoint
+from .attribute_statement_mapping_rule import AttributeStatementMappingRule, AttributeStatementMappingRules
 from .auth_token import AuthToken, AuthTokenList
+from .base import Expansion, Resource, CollectionResource, SaveMixin, DeleteMixin, AutoSaveMixin
+from .challenge import Challenge, ChallengeList
+from .custom_data import CustomData
 from .default_relay_state import DefaultRelayState, DefaultRelayStateList
+from .directory import Directory
+from .factor import Factor, FactorList
 from .field import Field
 from .group import Group, GroupList
-from .provider import Provider
-from .tenant import Tenant
-from .directory import Directory
 from .group_membership import GroupMembership, GroupMembershipList
-from .custom_data import CustomData
-from .base import (Expansion, Resource, CollectionResource,
-    SaveMixin, DeleteMixin, AutoSaveMixin)
+from .organization import Organization, OrganizationList
+from .organization_account_store_mapping import OrganizationAccountStoreMapping, OrganizationAccountStoreMappingList
 from .password_reset_token import PasswordResetTokenList
+from .phone import Phone, PhoneList
+from .provider import Provider
 from .saml_policy import SamlPolicy
 from .saml_service_provider import SamlServiceProvider
 from .saml_service_provider_metadata import SamlServiceProviderMetadata
 from .sso_initiation_endpoint import SsoInitiationEndpoint
-from .organization import Organization, OrganizationList
-from .organization_account_store_mapping import (
-    OrganizationAccountStoreMapping,
-    OrganizationAccountStoreMappingList)
-from .factor import Factor, FactorList
-from .phone import Phone, PhoneList
-from .challenge import Challenge, ChallengeList
+from .tenant import Tenant
